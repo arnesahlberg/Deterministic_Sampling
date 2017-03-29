718929a4b4096b4f74afcb1b8615a08170e4789d
Arne Sahlberg <arne.sahlberg@gmail.com>
2017-03-29 10:35:45 +0100

    Added mergePoints.m

---
+++ functions/operations_on_ensembles/mergePoints.m
@@ -0,0 +1,17 @@
+%merge two points in an ensemble and return new ensemble 
+function [q2,w2] = mergePoints(q,w,i1,i2)
+
+qt = [q(i1,:) ; q(i2,:)] ;
+wt = [w(i1,:) ; w(i2,:)] ;
+
+q2 = q ;
+w2 = w ;
+
+q2(i2) = []
+w2(i2) = []
+q2(i1) = weightedMean(qt,wt)
+w2(i1) = sum(wt)
+
+end
+
+
